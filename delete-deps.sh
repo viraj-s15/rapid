e0b51ba421d5a4536fd40ce1f95bdf09e56ac932
viraj-s15 <veer15102003@gmail.com>
2023-11-13 21:07:53 +0530

    initial commit

---
+++ delete-deps.sh
@@ -0,0 +1,16 @@
+cd src-tauri
+rm -rf tauri 
+cd target 
+cd debug
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+rm -rfv deps
+cd .. 
+cd .. 
+rm -rf target
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,33 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
+    tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "close" => {
+                event.window().close().unwrap();
+            }
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
